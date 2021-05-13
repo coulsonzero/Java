180b1e2da812ccd7d98516f16ac987bdb64a2d8d
coulsonzero <82859512+coulsonzero@users.noreply.github.com>
2021-05-13 18:14:23 +0800

    Update 整数反转.py

--- LeetCode/整数反转.py
+++ LeetCode/整数反转.py
@@ -1,11 +1,17 @@
+# 字符串反转方法
+
 class Solution:
     def reverse(self, x: int) -> int:
         s = int(str(x)[::-1]) if x>=0 else -int(str(x)[1:][::-1])
         return s if -2**31 <= s <= 2**31-1 else 0
-        # s = int(str(abs(x))[::-1])
-        # if -2**31 <= s <= 2**31-1: 
-        #     return s if x >= 0 else -s
-        # return 0
+    
+        '''
+        abs效率太低，执行时间提高
+        s = int(str(abs(x))[::-1])
+        if -2**31 <= s <= 2**31-1: 
+            return s if x >= 0 else -s
+        return 0
+        '''
 
 
 '''
