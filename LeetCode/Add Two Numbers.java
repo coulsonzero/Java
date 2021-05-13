da6fb1810e9d1b63203220f23d922415a63b824f
coulsonzero <82859512+coulsonzero@users.noreply.github.com>
2021-05-13 13:48:41 +0800

    Update Add Two Numbers.java

--- LeetCode/Add Two Numbers.java
+++ LeetCode/Add Two Numbers.java
@@ -1,3 +1,14 @@
+/**
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+*/
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         return this.addTwoNumbers2(l1,l2,0);
