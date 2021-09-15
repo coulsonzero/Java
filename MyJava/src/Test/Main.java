25b37b0f5ecd21ed55c64377d1318dc9ecf7d208
coulsonzero <coulson2021@163.com>
2021-09-15 23:05:44 +0800

    09/15

--- MyJava/src/Test/Main.java
+++ MyJava/src/Test/Main.java
@@ -1,9 +1,18 @@
-/**
- * @author coulson
- * @version 2021-09-08 21:00
- */
+import java.lang.invoke.SwitchPoint;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hhelo");
+        int m = 1;
+        switch (m) {
+            case 0:  System.out.println("condition 0");
+            case 1:  System.out.println("condition 1");
+            case 2:  System.out.println("condition 2");
+            case 3: System.out.println("condition 3");
+                break;
+            case 4:  System.out.println("condition 4");
+            default: System.out.println("condition default");
+        }
+
     }
 }
